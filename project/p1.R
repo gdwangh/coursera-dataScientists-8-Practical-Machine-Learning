36fd8248367c73c21a7d3b903542355907903b40
gdwangh <gdwangh@189.cn>
2014-11-14 18:26:40 +0800

    update course project: add relationship between variable come from belt sensors

--- project/p1.R
+++ project/p1.R
@@ -45,3 +45,30 @@ nearZeroVar(train_ds[,clist], saveMetrics=TRUE)
 featurePlot(x=train_ds[,c(6,7,9)], y=train_ds[,160], plot="pairs")
 classe
 
+# 检查NA值很多的那些列是什么回事
+# max_roll_belt等多NA字段，与num_window，new_window的关系 
+qplot(train_ds$num_window,train_ds$new_window, col=is.na(train_ds$max_roll_belt))
+# 观察结果显示：
+# new_window = yes --- max，min等计算属性有值
+#            = no --- max，min等计算属性=NA
+
+t<-table(train_ds$num_window,train_ds$new_window)  
+
+# 有些num_windows没有new_windows, 有些num_windows有1个new_windows=yes，不会有2、3、4个
+
+# new_window 与 classe的关系,貌似没有直接关系
+qplot(train_ds$num_window,train_ds$classe, col=train_ds$new_window)
+
+#  belt sensors相关各参数之间的关系
+M<-abs(cor(train_ds[,c(8:11,37:45)]))
+diag(M)<-0
+which(M>0.8,arr.ind=T)
+
+# train 执行出错，因为 glm只能针对2-level factor
+preProc_belt<-preProcess(train_ds[,c(8:11,37:45)], method="pca", thresh=0.8)
+trainPC_belt<-predict(preProc_belt, train_ds[,c(8:11,37:45)])
+fit1<-train(train_ds$classe~., method="glm",data=trainPC_belt,family="binomial")
+
+fit1<-t(train_ds$classe ~ ., data=trainPC_belt, family="binomial")
+dim(trainPC_belt)
+
